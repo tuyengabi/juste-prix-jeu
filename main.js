e6a243838f24bd605c7a5a278aa1d0e1061c8654
tuyen.gabi <tuyen.gabi@gmail.com>
2019-10-17 10:03:23 +0200

    add media query for smartphone

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
 
-//  CAROUSEL TEXT 
-
-const carousel = document.getElementsByClassName('carousel__text')[0];
-let arrLocation = Array.from(document.getElementsByClassName('carousel__location'));
-let count = 0;
-
-// FUNCTION OF CAROUSEL TEXT
-
-
-function toggleClassHidden(index) {
-    return arrLocation[index].classList.toggle('carousel__location--hidden');
-}
-
-function verifyCount(arr, number) {
-    if (number == arr.length) {
-        return count = 0;
-    }
-    if (number < 0) {
-        return count = arr.length - 1;
-    } else {
-        return count;
-    }
-}
 
 
