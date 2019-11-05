7c8a25b798d2525c219b2a55ac1f2c349ef243bf
tuyengabi <tuyen.gabi@gmail.com>
2019-11-05 10:49:04 +0100

    add file index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("btnStart").addEventListener("click", function () {
+    document.getElementById("hint").innerHTML = "est entre 700 et 800";
+});
+
+document.getElementById("btnStart").addEventListener("click", function () {
+    document.getElementById("count").innerHTML = "30";
+});
+
+document.getElementById("btnStart").addEventListener("click", function () {
+    document.getElementById("text").innerHTML = "secondes";
+});
+
+document.getElementById("btnStart").addEventListener("click", function () {
+    document.getElementById("input").style.visibility = "visible";
+});
+
+
