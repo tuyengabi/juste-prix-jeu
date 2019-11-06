1dba2b21278c203a08f3a5cff7ead727ec53b421
tuyengabi <tuyen.gabi@gmail.com>
2019-11-06 16:25:44 +0100

    add function setInterval

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
+// let timer = countdown();
 
+
+
+// document.getElementById("btnStart").addEventListener("click", function () {
+//     document.getElementById("count").innerHTML = timer;
+// });
+
+
+
+// function countdown() {
+
+//     for (let sec = 30; sec > 0; sec--) {
+//         console.log(sec);
+//     }
+
+    
+// }
+
+
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
 document.getElementById("btnStart").addEventListener("click", function () {
-    document.getElementById("count").innerHTML = "30";
+    var fiveMinutes = 60 * 0.5,
+        display = document.querySelector('#count');
+    startTimer(fiveMinutes, display);
 });
 
 document.getElementById("btnStart").addEventListener("click", function () {
@@ -19,6 +56,8 @@ document.getElementById("btnStart").addEventListener("click", function () {
 document.getElementById("btnStart").addEventListener("click", function () {
     document.getElementById("btnStart").innerHTML = "OK";
 });
+
+
 
 
 
